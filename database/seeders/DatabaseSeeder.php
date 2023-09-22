550375d095ed17c48ac4fe0f4eb4e8f453b2c38f
clavene <claudia.422022018@civitas.ukrida.ac.id>
2023-09-22 12:01:13 +0700

    Seeder: admin users, camps, camp_benefits

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,11 +9,17 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     * 
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
         // \App\Models\User::factory(10)->create();
-
+        $this->call([
+            CampTableSeeder::class,
+            CampBenefitTableSeeder::class,
+            AdminUserSeeder::class,
+        ]);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
